5d1b42f028dac4bde112c5b4505d809b86231825
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-07-01 18:59:44 +0100

    Created queries.js This will hold connection information and functions for SQL queries

---
+++ db/queries.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2')
+
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '***REMOVED***',
+        database: 'employee_db'
+      },
+      console.log('Connected to the employee_db database.')
+)
