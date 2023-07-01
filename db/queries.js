d2e06f420a15ad64f952b78e1d4eba390000c106
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-07-01 20:17:25 +0100

    Added selectEmployees method Also reformatted selectRoles method so the query isn't all on one line

--- db/queries.js
+++ db/queries.js
@@ -24,7 +24,26 @@ class Queries {
 
     async selectRoles() {
         try {
-            const [rows, fields] = await db.query('SELECT role.id, role.title, department.name as department, role.salary FROM role INNER JOIN department ON role.department_id = department.id')
+            const [rows, fields] = await db.query(
+                `SELECT role.id, role.title, department.name as department, role.salary
+                FROM role
+                INNER JOIN department ON role.department_id = department.id`
+            )
+            return rows
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async selectEmployees() {
+        try {
+            const [rows, fields] = await db.query(
+                `SELECT employee.*, role.title, department.name, role.salary, manager.first_name as manager_first_name, manager.last_name as manager_last_name
+                FROM employee
+                INNER JOIN role ON employee.role_id = role.id
+                INNER JOIN department ON role.department_id = department.id
+                LEFT JOIN employee AS manager on employee.manager_id = manager.id`
+            )
             return rows
         } catch (err) {
             throw err
