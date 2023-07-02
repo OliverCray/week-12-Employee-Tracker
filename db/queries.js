3abe59063b5dfc1e95261e4578d18acaa734780e
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-07-02 15:09:57 +0100

    Added comments describing methods

--- db/queries.js
+++ db/queries.js
@@ -60,7 +60,7 @@ class Queries {
     async selectEmployeesQuery() {
         try {
             const [rows, fields] = await db.query(
-                // Select all employees and their respective role, department and manager
+                // Select all employees and their respective role, department, salary and manager
                 // LEFT JOIN ensures that the employee information appears even if their role or department has been deleted
                 `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary,
                 CONCAT(manager.first_name, ' ', manager.last_name) AS manager
@@ -78,6 +78,7 @@ class Queries {
     async selectByManagerQuery(employee) {
         try {
             const [rows, fields] = await db.query(
+                // Select employees based on chosen manager, also displays their role, department and salary
                 `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary
                 FROM employee 
                 LEFT JOIN role ON employee.role_id = role.id
@@ -94,6 +95,7 @@ class Queries {
     async selectByDepartmentQuery(department) {
         try {
             const [rows, fields] = await db.query(
+                // Select employees based on chosen department, also displays their role, salary and manager
                 `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, 
                 CONCAT(manager.first_name, ' ', manager.last_name) AS manager
                 FROM employee
@@ -111,6 +113,7 @@ class Queries {
     async selectCombinedSalaryQuery(department) {
         try {
             const [rows, fields] = await db.query(
+                // Select the combined salary of the employees for the chosen department
                 `SELECT department.name as department, SUM(role.salary) AS "total salary"
                 FROM role
                 INNER JOIN employee ON employee.role_id = role.id
@@ -125,6 +128,7 @@ class Queries {
 
     async addDepartmentQuery(department) {
         try {
+            // Adds a new department to the database
             await db.query('INSERT INTO department (name) VALUES (?)', [department.name])
             console.log(`Added ${department.name} to the list of departments`)
         } catch (err) {
@@ -134,6 +138,7 @@ class Queries {
 
     async addRoleQuery(role) {
         try {
+            // Adds a new role to the database
             await db.query('INSERT INTO role (title, salary, department_id) VALUES (?,?,?)', [role.title, role.salary, role.department_id])
             console.log(`Added ${role.title} to the list of roles`)
         } catch (err) {
@@ -144,6 +149,7 @@ class Queries {
     async addEmployeeQuery(employee) {
         try {
             await db.query(
+                // Adds a new employee to the database
                 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)', 
                 [employee.first_name, employee.last_name, employee.role_id, employee.manager_id]
             )
@@ -155,6 +161,7 @@ class Queries {
 
     async updateEmployeeRoleQuery(employee) {
         try {
+            // Updates an employee's role
             await db.query('UPDATE employee SET role_id = ? WHERE id = ?', [employee.role_id, employee.employee_id])
             console.log(`Updated employee's role`)
         } catch (err) {
@@ -164,6 +171,7 @@ class Queries {
 
     async updateEmployeeManagerQuery(employee) {
         try {
+            // Updates an employee's manager
             await db.query('UPDATE employee SET manager_id = ? WHERE id = ?', [employee.manager_id, employee.employee_id])
             console.log(`Updated employee's manager`)
         } catch (err) {
@@ -173,6 +181,7 @@ class Queries {
 
     async deleteDepartmentQuery(department) {
         try {
+            // Delete the chosen department from the database
             await db.query('DELETE FROM department WHERE id = ?', [department.department_id])
             console.log('Department deleted')
         } catch (err) {
@@ -182,6 +191,7 @@ class Queries {
     
     async deleteRoleQuery(role) {
         try {
+            // Delete the chosen role from the database
             await db.query('DELETE FROM role WHERE id = ?', [role.role_id])
             console.log('Role deleted')
         } catch (err) {
@@ -191,6 +201,7 @@ class Queries {
 
     async deleteEmployeeQuery(employee) {
         try {
+            // Delete the chosen employee from the database
             await db.query('DELETE FROM employee WHERE id = ?', [employee.employee_id])
             console.log('Employee deleted')
         } catch (err) {
